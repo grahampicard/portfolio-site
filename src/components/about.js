c853cfa15e88da44fe243697ee7b3a9bce64628a
Graham Picard <grahampicard@gmail.com>
2017-09-30 13:37:24 -0400

    updated footer

--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import { Link, NavLink } from "react-router-dom";
 import aboutMePic from "./img/about-me-pic.png";
-import LinkedIn from "./svg/linkedin";
-import Twitter from "./svg/twitter";
-import Github from "./svg/github";
+import Footer from "./footer";
 
 
 export default class About extends Component {
@@ -22,18 +20,15 @@ export default class About extends Component {
             <img src={aboutMePic} alt="Headshot" className="headshot" />
             <p>Thanks for visiting my homepage.</p>
             <p>I am an Insight Analyst/Business
-               Intelligence Developer, with a focus on full-stack dashboard
-               development and an interest in making data easier to digest for
-               wide audiences.
-            </p>
-            <p>I drive strategy through analysis and interactive
-               data tools.
+               Intelligence Developer, focusing on end-to-end analysis projects.
+               I work at the intersection of data and strategy, and I'm intereseted
+               in making data easier for wide audiences to digest.
             </p>
             <p>I'm also a former high school teacher and a lifelong musician.
                View my interactive <NavLink exact to="/resume">resume</NavLink> and
                visit some of my side projects, like my <NavLink exact to="/portfolio/spotify-songkick-artist-tool"> Spotify & Songking Artist Tool </NavLink> to get to know what interests me.
             </p>
-            <p>Contact me directly at <a href="mailto:grahampicard@gmail.com">
+            <p>Send me an email at <a href="mailto:grahampicard@gmail.com">
                 grahampicard@gmail.com</a>.
             </p>
           </div>
@@ -62,12 +57,7 @@ export default class About extends Component {
             <li>Music production</li>
           </div>
         </div>
-        <ul className="socialMedia">
-          <li><a href="https://www.linkedin.com/in/graham-picard/" target="_blank"><LinkedIn /></a></li>
-          <li><a href="https://twitter.com/grahampicard/" target="_blank"><Twitter /></a></li>
-          <li><a href="https://www.github.com/grahampicard/" target="_blank"><Github /></a></li>
-        </ul>
-        <p><em>Created with React & d3. Hosted on AWS.</em></p>
+        <Footer />
       </div>
     )
   }
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link, NavLink } from "react-router-dom";
+import Footer from "./footer";
 
 
 export default class Test extends Component {
@@ -11,21 +12,22 @@ export default class Test extends Component {
     return(
       <div className="welcome">
         <span className="welcome-text">Welcome</span>
-        <h2>Busy? I get it. Here's how to make the most of your visit.</h2>
+        <h2>Busy? Make the most of your visit. </h2>
         <div className="timings">
           <div className="minute one">
-            <h2>Have a minute?</h2>
-            <p>Check out my interactive <NavLink exact to="/resume">resume</NavLink>. Make sure to click around the timeline.</p>
+            <h2>One minute?</h2>
+            <p>Check out my interactive <NavLink exact to="/resume">resume</NavLink>. Click around the timeline.</p>
           </div>
           <div className="minute two">
             <h2>Two minutes?</h2>
-            <p>After you played with my <NavLink exact to="/resume">resume</NavLink>, click through my <NavLink exact to="/portfolio">portfolio</NavLink> of data products. Intested in music, sports, or education? There's a project that might catch your eye.</p>
+            <p>After playing with my <NavLink exact to="/resume">resume</NavLink>, find one of my <NavLink exact to="/portfolio">side projects</NavLink> that catches your eye.</p>
           </div>
           <div className="minute two">
             <h2>Five minutes?</h2>
-            <p>Click through the full site and get to know a little more about me on the <NavLink exact to="/about">about</NavLink> page.</p>
+            <p>See the whole site, and get to know me the <NavLink exact to="/about">about</NavLink> page.</p>
           </div>
         </div>
+        <Footer />
       </div>
     )
   }
